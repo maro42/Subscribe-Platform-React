e622a16ac839daf4e60e888d7d82c571cc8cf572
bang <maro42@naver.com>
2021-08-24 22:48:54 +0900

    feat: 로그인 시 성공/실패/권한처리

--- src/lib/api/auth.tsx
+++ src/lib/api/auth.tsx
@@ -1,12 +1,12 @@
 import { login } from "../props/auth";
 import client from "./client";
 
-export const getLoginToken = ({email, password}:login) => client.post('/auth/login', 
-{
-        'email' : email,
-        'password' : password
+export const getLoginToken = ({ email, password }: login) => client.post('/auth/login',
+    {
+        'email': email,
+        'password': password
     }
 );
 
-export const getUserId = ({id}:any) =>
-    client.get('/user/'+id);
+export const getUserId = ({ id }: any) =>
+    client.get('/user/' + id);
