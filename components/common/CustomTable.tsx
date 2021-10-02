95fac2e269da6503b03116afd0a319697f8e87fc
doohee94 <doohee94@naver.com>
2021-10-02 10:55:00 +0900

    feat: service 상세 화면 구현

--- components/common/CustomTable.tsx
+++ components/common/CustomTable.tsx
@@ -2,65 +2,52 @@ import React, { useMemo } from 'react';
 import Table from '@material-ui/core/Table';
 import TableContainer from '@material-ui/core/TableContainer';
 import Paper from '@material-ui/core/Paper';
-import { TableCell, TableHead,TableBody, TableRow } from '@material-ui/core';
-import {TableHeader} from '../../src/lib/props'
+import { TableCell, TableHead, TableBody, TableRow } from '@material-ui/core';
+import { TableHeader } from '../../src/lib/props';
 
-
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
   return { name, calories, fat, carbs, protein };
 }
 
 //TODO : 페이지네이션 추가
 
 type CustomTableProps = {
-    headers : TableHeader[];
-    content : {[key:string] : string }[];
-}
+  headers: TableHeader[];
+  content: { [key: string]: string }[];
+};
 
-export default function CustomTable(
-    {
-        headers,
-        content,
-    }: CustomTableProps
-) {
-
-
-
+export default function CustomTable({ headers, content }: CustomTableProps) {
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
-              { <TableRow>
-                    {
-                   headers.map((v,index)=>{
-                       return (
-                           <TableCell key={index}>{v.label}</TableCell>
-                       )
-                   })
-               }
-                </TableRow>}
+          {
+            <TableRow>
+              {headers.map((v, index) => {
+                return <TableCell key={index}>{v.label}</TableCell>;
+              })}
+            </TableRow>
+          }
         </TableHead>
         <TableBody>
-            { content.map((v, index)=>{
-
-return(
-    <TableRow key={index}>
-        
-        {
-           headers.map((h, i)=>{
-               //TODO : align 추가 
-               return(
-                   <TableCell key={i}>{v[h.property]}</TableCell>
-               )
-           })
-        }
-
-    </TableRow>
-)
-
-})}
-        </TableBody>     
-    </Table>
+          {content.map((v, index) => {
+            return (
+              <TableRow key={index}>
+                {headers.map((h, i) => {
+                  //TODO : align 추가
+                  return <TableCell key={i}>{v[h.property]}</TableCell>;
+                })}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
     </TableContainer>
   );
 }
--- components/store/products/ServiceTable.tsx
+++ components/store/products/ServiceTable.tsx
@@ -1,7 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Table from '@material-ui/core/Table';
+import TableContainer from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
+import { TableHeader } from '../../../src/lib/props';
+import {
+  TableCell,
+  TableHead,
+  TableBody,
+  TableRow,
+  Collapse,
+  Button,
+} from '@material-ui/core';
 
-function ServiceTable() {
-  return <>service table</>;
+type ServiceTableRowProps = {
+  headers: TableHeader[];
+  content: { [key: string]: string | any };
+};
+
+function ServiceTableRow({ headers, content }: ServiceTableRowProps) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow hover>
+        {headers.map((h, i) => {
+          if (h.property === 'title') {
+            return (
+              <TableCell key={i}>
+                <Button onClick={() => setOpen(!open)}>
+                  {content[h.property]}
+                </Button>
+              </TableCell>
+            );
+          }
+          return <TableCell key={i}>{content[h.property]}</TableCell>;
+        })}
+      </TableRow>
+      <TableRow>
+        <TableCell
+          colSpan={headers.length}
+          style={{ paddingBottom: 0, paddingTop: 0 }}
+        >
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <div>
+              {content['custom'].title}
+              {content['custom'].content}
+            </div>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+type ServiceTableProps = {
+  data: {
+    headers: TableHeader[];
+    content: { [key: string]: string | any }[];
+  };
+};
+
+function ServiceTable({ data }: ServiceTableProps) {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          {
+            <TableRow>
+              {data.headers.map((v, index) => {
+                return <TableCell key={index}>{v.label}</TableCell>;
+              })}
+            </TableRow>
+          }
+        </TableHead>
+        <TableBody>
+          {data.content.map((v, index) => {
+            if (v['custom']) {
+              return <ServiceTableRow content={v} headers={data.headers} />;
+            } else {
+              return (
+                <React.Fragment>
+                  <TableRow key={index} hover>
+                    {data.headers.map((h, i) => {
+                      //TODO : align 추가
+                      return <TableCell key={i}>{v[h.property]}</TableCell>;
+                    })}
+                  </TableRow>
+                </React.Fragment>
+              );
+            }
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default ServiceTable;
